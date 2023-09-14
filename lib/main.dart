317d4c5bbaa21e57e00c4fb4fb0c14cf660d0ad3
Shashank-btc <shashanks@boston-technology.com>
2023-09-14 12:45:44 +0530

    scaffold and MaterialApp

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,22 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    Center(
-      child: Text("Hello world",textDirection:TextDirection.ltr,),
+    MaterialApp(
+      title: "My App",
+      color: Colors.red,
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+        brightness: Brightness.light
+      ),
+      // this is the widgt which help use to build screen the use all the widgts
+      home: Scaffold(
+        appBar: AppBar(title: Text("My First Flutter App",),
+        ),
+        floatingActionButton : FloatingActionButton(onPressed: null,
+        child: Text("clicked"),),
+        body: Center(child: Text("My first flutter programmer :)")),
+      ),
     )
   );
 }
